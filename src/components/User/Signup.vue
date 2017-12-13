8a64a4c120356569675a594716c3ff32fb609eee
Angel Angeles <aangeles@argentuminc.com>
2017-12-13 17:23:33 -0400

    vue first commit

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -46,7 +46,7 @@
                 </v-layout>
                 <v-layout row>
                   <v-flex xs12>
-                    <v-btn type="submit" :disabled="loading" :loading="loading">
+                    <v-btn type="submit" :disabled="false" :loading="false">
                       Sign up
                        <span slot="loader" class="custom-loader">
                         <v-icon light>cached</v-icon>
